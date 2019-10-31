2cc9c3d3fc478f116a56da84b1231790078ecf39
williamjzhao <wzhao2017@gmail.com>
2019-10-31 18:55:25 -0500

    adding dependencies

--- src/components/box.js
+++ src/components/box.js
@@ -14,14 +14,17 @@ const Box = props => {
             </div>
             <div className='desc'>{props.description}</div>
             <div className='preqs'>{props.preqs}</div>
-            <div>
+            <div className='deps'>
+                <i>Dependencies: {props.dependencies}</i>
+            </div>
+            <div className='github'>
                 <span>{props.github ? "Code: " : ""}</span>
 
                 <a className='github' href={props.github}>
                     {props.github}
                 </a>
             </div>
-            <div>
+            <div className='github'>
                 <span>{props.slides ? "Slides: " : ""}</span>
                 <a className='github' href={props.slides}>
                     {props.slides}
