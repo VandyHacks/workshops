653aa3149809159a30a64cb1ff8b69fcd71e6a9f
xuezzou <ilfam1999@gmail.com>
2020-08-19 14:48:07 -0500

    remove styled components

--- src/components/cardContainer.js
+++ src/components/cardContainer.js
@@ -1,19 +1,12 @@
 import PropTypes from "prop-types"
 import React from "react"
-import styled from 'styled-components'
 
 import Card from "./card"
 
 // for styling
 import "./skeleton.css"
+import "./cardContainer.css"
 
-
-const CardRow = styled.div`
-  margin: 3rem;
-  @media (max-width: 550px) {
-    margin: 0;
-  }
-`
 
 const createCards = (cards) => cards.map( card => (
   <div className="one-third column" key = {card.title}>
@@ -26,15 +19,15 @@ const CardContainer = (props) => {
   const cardsHTML = props.cards.map((card, i) => {
     if((i + 1) % 3 === 0) {
       return (
-        <CardRow className="row" key={i}>
+        <div className="row cards" key={i}>
           {createCards(props.cards.slice(i - 2, i + 1))}
-        </CardRow>
+        </div>
       ) 
     } else if (i === props.cards.length - 1 ) {
       return (
-        <CardRow className="row" key={i}>
+        <div className="row cards" key={i}>
          {createCards(props.cards.slice(Math.floor(i / 3) * 3, props.cards.length))}
-        </CardRow>
+        </div>
       ) 
     }
   })
